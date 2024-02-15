bab9e2d53100074a8fa81ae94faeb72dca4e4085
Your Name <you@example>
2024-02-16 00:02:02 +0800

    build_.net.std.2.0_with_global.json_.net8

--- test.fsx
+++ test.fsx
@@ -1,5 +1,5 @@
 //#r @"E:\ExcelProvider\src\ExcelProvider.Runtime\bin\Debug\netstandard2.0\ExcelProvider.Runtime.dll"
-#r @"G:\coldfar_py\ExcelProvider\src\ExcelProvider.Runtime\bin\Debug\netstandard2.0\ExcelProvider.Runtime.dll"
+#r @"G:\coldfar_py\ExcelProvider\src\ExcelProvider.Runtime\bin\Release\netstandard2.0\ExcelProvider.Runtime.dll"
 open FSharp.Interop.Excel
 
 [<Literal>]
